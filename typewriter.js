0ad9135acdde687f5bf23a4f542cdb4924002daa
Umang Patel <umangpatel1991.cmail.com>
2021-09-21 14:20:57 -0400

    coded to print string characters after 50ms to command promts on same line

---
+++ typewriter.js
@@ -0,0 +1,10 @@
+const sentence = "hello there from lighthouse labs";
+
+let counter = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+
+  }, 50 * counter);
+  counter++;
+}
